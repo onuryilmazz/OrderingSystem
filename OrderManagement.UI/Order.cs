04e36d8ec0bce5f36eba1541ad83694d11a853a3
onuryilmaz90 <onuryilmaz90@outlook.com>
2023-10-02 08:23:19 +0200

    - Solution and OrderManagement.UI project are added.

---
+++ OrderManagement.UI/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public sealed class Order
+{
+    public Guid OrderId { get; set; }
+
+    public string ItemName { get; set; } = default!;
+
+    public string CustomerName { get; set; } = default!;
+
+    public decimal Amount { get; set; }
+
+    public OrderStatus Status { get; set; }
+}
+
+public enum OrderStatus
+{
+    New,
+    Processing,
+    Shipped,
+    Delivered,
+    Cancelled
+}
